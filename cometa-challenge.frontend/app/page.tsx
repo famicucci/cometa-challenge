f0bbfc461820400f4292a111092679669a97ee72
Francisco A. Micucci <francisco.micucci@diveria.com>
2025-03-16 19:00:44 -0300

    remove grid

--- cometa-challenge.frontend/app/page.tsx
+++ cometa-challenge.frontend/app/page.tsx
@@ -1,15 +1,14 @@
-import { Typography } from "./components/designSystemComponents/Typography";
 import { Order } from "./components/Order";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main>
+    <div>
+      <main className="p-5 max-w-3xl m-auto">
         <Order />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <Typography variant="body medium">Francisco Micucci</Typography>
-      </footer>
+      </footer> */}
     </div>
   );
 }
