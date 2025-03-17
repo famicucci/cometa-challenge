f8976baaff3211106e84bde418e7592f12d31391
Francisco A. Micucci <francisco.micucci@diveria.com>
2025-03-17 19:38:38 -0300

    create initial order

--- cometa-challenge.backend/mocks/orders.py
+++ cometa-challenge.backend/mocks/orders.py
@@ -2,12 +2,75 @@
 order = {
     "created": "2024-09-10 12:00:00",
     "paid": False,
-    "subtotal": 0,
-    "taxes": 0,
+    "subtotal": 1050,
+    "taxes": 105,
     "discounts": 0,
-    "items": [],
-    "rounds": []
+    "items": [
+        {
+            "name": "Quilmes",
+            "price_per_unit": 120,
+            "total": 5
+        },
+        {
+            "name": "Club Colombia",
+            "price_per_unit": 110,
+            "total": 2
+        },
+        {
+            "name": "Corona",
+            "price_per_unit": 115,
+            "total": 2
+        }
+    ],
+    "rounds": [
+        {
+            "created": "2024-09-10 12:00:30",
+            "items": [
+                {
+                    "name": "Corona",
+                    "quantity": 2
+                },
+                {
+                    "name": "Club Colombia",
+                    "quantity": 1
+                }
+            ]
+        },
+        {
+            "created": "2024-09-10 12:20:31",
+            "items": [
+                {
+                    "name": "Club Colombia",
+                    "quantity": 1
+                },
+                {
+                    "name": "Quilmes",
+                    "price": 2
+                }
+            ]
+        },
+        {
+            "created": "2024-09-10 12:43:21",
+            "items": [
+                {
+                    "name": "Quilmes",
+                    "quantity": 3
+                }
+            ]
+        }
+    ]
 }
+
+# empty order
+# order = {
+#     "created": "2024-09-10 12:00:00",
+#     "paid": False,
+#     "subtotal": 0,
+#     "taxes": 0,
+#     "discounts": 0,
+#     "items": [],
+#     "rounds": []
+# }
 
 # rounds
 # {
