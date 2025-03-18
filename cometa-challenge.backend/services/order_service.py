007804381ee19c8a3525f4722c9fbdbb660a8f04
Francisco A. Micucci <famicucci@gmail.com>
2025-03-17 21:33:32 -0300

    refactor add round

--- cometa-challenge.backend/services/order_service.py
+++ cometa-challenge.backend/services/order_service.py
@@ -11,17 +11,17 @@ def add_round_to_order(round_items: list[RoundItem]):
     order["rounds"].append(round_data)
 
     # Actualizar subtotal y items de la orden
-    for item in round_items:
-        order["subtotal"] += item.quantity * next(beer for beer in stock["beers"] if beer["name"] == item.name)["price"]
-
     for round_item in round_items:
+        beer = next(beer for beer in stock["beers"] if beer["name"] == round_item.name)
+        order["subtotal"] += round_item.quantity * beer["price"]
+
         existing_item = next((item for item in order["items"] if item["name"] == round_item.name), None)
         if existing_item:
             existing_item["total"] += round_item.quantity
         else:
             order["items"].append({
                 "name": round_item.name,
-                "price_per_unit": next(beer for beer in stock["beers"] if beer["name"] == round_item.name)["price"],
+                "price_per_unit": beer["price"],
                 "total": round_item.quantity
             })
 
